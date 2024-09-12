fb0e1be9878644e4089702f2f2fed7bf04e7dd51
Leonel <leonelezequiel468@gmail.com>
2024-09-12 15:20:24 -0300

    correccion en esquematico agrego imagenes de ganancias de lazo

--- graficos.py
+++ graficos.py
@@ -15,9 +15,12 @@ def leer_datos(archivo):
 
 
 # Función para graficar los datos
-def graficar(data1, data2, color='b', xlabel='', ylabel='', title='', label=''):
+def graficar(data1, data2, color='b', xlabel='', ylabel='', title='', label='', semilogx=False):
     plt.figure(figsize=(10, 6))
-    plt.plot(data1, data2, linestyle='-', color=color, label=label)
+    if semilogx:
+        plt.semilogx(data1, data2, linestyle='-', color=color, label=label)
+    else:
+        plt.plot(data1, data2, linestyle='-', color=color, label=label)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -37,7 +40,10 @@ eficiencia_3_3_B_txt = 'eficiencia3_3B.txt'
 eficiencia_5_B_txt = 'eficiencia5B.txt'
 regulacion_linea_3_3B_txt = 'regulacion_linea_3_3B.txt'
 regulacion_linea_5B_txt = 'regulacion_linea_5B.txt'
-
+ganancia_lazo_5B_txt = 'ganancia_de_lazo_5_B.txt'
+ganancia_lazo_3_3B_txt = 'ganancia_de_lazo_3_3_B.txt'
+a_modificado_3_3B_txt = 'a_modificado_3_3B.txt'
+a_modificado_5B_txt = 'a_modificado_5B.txt'
 
 # Leer los datos del archivo
 tiempo1, Vo_5 = leer_datos(vo_5_B_txt)
@@ -48,6 +54,10 @@ vin1, eficiencia_3_3B = leer_datos(eficiencia_3_3_B_txt)
 vin2, eficiencia_5_B = leer_datos(eficiencia_5_B_txt)
 vin3, regulacion_linea_3_3B = leer_datos(regulacion_linea_3_3B_txt)
 vin4, regulacion_linea_5B = leer_datos(regulacion_linea_5B_txt)
+freq1, ganancia_lazo_5B = leer_datos(ganancia_lazo_5B_txt)
+freq2, ganancia_lazo_3_3B = leer_datos(ganancia_lazo_3_3B_txt)
+freq3, a_modificado_5B = leer_datos(a_modificado_5B_txt)
+freq4, a_modificado_3_3B = leer_datos(a_modificado_3_3B_txt)
 
 # Graficar los datos
 graficar(irl_5_B, Vo_5, 'b',
@@ -79,3 +89,23 @@ graficar(vin4, regulacion_linea_5B, 'b',
          r'$V_{in} [V]$', r'V_o [V]',
          'Regulación de linea del regulador B 5V',
          'Regulación de linea')
+
+graficar(freq1, ganancia_lazo_5B, 'b',
+         'Frecuencia [Hz]', 'af [dB]',
+         'Ganancia de lazo a 5V regulador B',
+         'Ganancia de lazo ', True)
+
+graficar(freq2, ganancia_lazo_3_3B, 'b',
+         'Frecuencia [Hz]', 'af [dB]',
+         'Ganancia de lazo a 3,3V regulador B',
+         'Ganancia de lazo ', True)
+
+graficar(freq3, a_modificado_5B, 'b',
+         'Frecuencia [Hz]', 'a modificado [dB]',
+         'Ganancia de amplificador modificado a 5V regulador B',
+         'Ganancia del amplificador modificado', True)
+
+graficar(freq4, ganancia_lazo_3_3B, 'b',
+         'Frecuencia [Hz]', 'a modificado [dB]',
+         'Ganancia del amplificador modificado 3,3V regulador B',
+         'Ganancia del amlificador modificado',True)
