ce22b366aef07d48c92d7a9271c58427ea81af82
G-Leonel-6 <legonzalez@fi.uba.ar>
2024-09-24 23:17:38 -0300

    agrego imagenes del regulador B

--- graficos.py
+++ graficos.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -17,16 +18,19 @@ def graficar(x, y, xlabel='', ylabel='', title='', ruta_salida=''):
     plt.savefig(ruta_salida)
 
 
-def graficar_subplots(x, y1, y2, xlabel='', ylabel1='', ylabel2='', ruta_salida=''):
-    fig, axs = plt.subplots(1, 2, sharex=True)
+def graficar_subplots(x, y1, y2, xlabel='', ylabel1='', ylabel2='', title='', ruta_salida=''):
+    fig, axs = plt.subplots(1, 2, sharex=True, figsize=(10, 6))
 
+    fig.suptitle(title,  fontsize=16)
     axs[0].grid()
+    axs[0].set_xlim([1, 1*10**7])
     axs[0].semilogx(x, y1, 'r')
     axs[0].set_xlabel(xlabel)
     axs[0].set_ylabel(ylabel1)
 
     axs[1].grid()
-    axs[1].semilogy(x, y2, 'b')
+    axs[1].set_xlim([1, 1 * 10 ** 7])
+    axs[1].semilogx(x, y2, 'b')
     axs[1].set_xlabel(xlabel)
     axs[1].set_ylabel(ylabel2)
 
@@ -37,8 +41,8 @@ carpeta = 'trazas/checkpoint2'
 carpeta_salida = 'imagenes/checkpoint2'
 
 # Cargar los datos desde un archivo de texto, saltando la primera línea
-nombres1 = ['time', 'potencia a 3,3']
-nombres2 = ['time', 'potencia a 5']
+nombres1 = ['time', 'potencia a 3,3', 'vo_3_3']
+nombres2 = ['time', 'potencia a 5', 'vo_5']
 nombres3 = ['freq', 'magnitud tension', 'fase tension']
 nombres4 = ['freq', 'magnitud corriente', 'fase corriente']
 nombres5 = ['time', 'respuesta transitoria V']
@@ -49,8 +53,8 @@ nombres8 = ['freq', 'magnitud I sin estabilizar', 'fase I sin estabilizar']
 potencia_B_3_3 = f'{carpeta}/potencia_B_3_3.txt'  # Reemplaza con la ruta de tu archivo
 potencia_B_5 = f'{carpeta}/potencia_B_5.txt'
 lazo_estabilizado_B_corriente = f'{carpeta}/lazo_estabilizado_B_corriente.txt'
-lazo_estabilizado_B_tension = f'{carpeta}/lazo_estabilizado_B_5.txt'
-respuesta_transitoria = f'{carpeta}/respuesta_transitoria.txt'
+lazo_estabilizado_B_tension = f'{carpeta}/lazo_estabilizado_B_tension.txt'
+respuesta_transitoria = f'{carpeta}/respuesta_transitoria_B.txt'
 corriente_estabilizada = f'{carpeta}/corriente_estabilizada_B.txt'
 lazo_V_sin_estabilizar = f'{carpeta}/lazo_V_sin_estabilizar.txt'
 lazo_I_sin_estabilizar = f'{carpeta}/lazo_I_sin_estabilizar.txt'
@@ -64,14 +68,28 @@ datos6 = pd.read_csv(corriente_estabilizada, sep='\t', skiprows=1, names=nombres
 datos7 = pd.read_csv(lazo_V_sin_estabilizar, sep='\t', skiprows=1, names=nombres7)
 datos8 = pd.read_csv(lazo_I_sin_estabilizar, sep='\t', skiprows=1, names=nombres8)
 
-graficar_subplots(datos3[nombres3[0]], datos3[nombres3[1]], datos3[nombres3[2]],'Hz', 'dB',
-                  'fase ', f'{carpeta_salida}/lazo_V_B_estabilizado.png')
 
-graficar_subplots(datos4[nombres4[0]], datos4[nombres4[1]], datos4[nombres4[2]],'Hz', 'dB',
-                  'fase ', f'{carpeta_salida}/lazo_I_B_estabilizado.png')
+fase3_unwrap = np.unwrap(np.radians(datos3[nombres3[2]]))
+fase3_unwrap = np.degrees(fase3_unwrap)
 
-graficar_subplots(datos7[nombres7[0]], datos7[nombres7[1]], datos7[nombres7[2]], 'Hz', 'dB',
-                  'fase ', f'{carpeta_salida}/lazo_V_B_sin_estabilizar.png')
+fase4_unwrap = np.unwrap(np.radians(datos4[nombres4[2]]))
+fase4_unwrap = np.degrees(fase4_unwrap)
 
-graficar_subplots(datos8[nombres8[0]], datos8[nombres8[1]], datos8[nombres8[2]], 'Hz', 'dB',
-                  'fase ', f'{carpeta_salida}/lazo_V_B_estabilizado.png')
+fase7_unwrap = np.unwrap(np.radians(datos7[nombres7[2]]))
+fase7_unwrap = np.degrees(fase7_unwrap)
+
+fase8_unwrap = np.unwrap(np.radians(datos8[nombres8[2]]))
+fase8_unwrap = np.degrees(fase8_unwrap)
+
+graficar_subplots(datos3[nombres3[0]], datos3[nombres3[1]], fase3_unwrap, 'Hz', 'dB',
+                  'fase ', 'Lazo de tensión estabilizado', f'{carpeta_salida}/lazo_V_B_estabilizado.png')
+
+graficar_subplots(datos4[nombres4[0]], datos4[nombres4[1]], fase4_unwrap, 'Hz', 'dB',
+                  'fase ', 'Lazo de corriente estabilizado', f'{carpeta_salida}/lazo_I_B_estabilizado.png')
+
+
+graficar_subplots(datos7[nombres7[0]], datos7[nombres7[1]], fase7_unwrap, 'Hz', 'dB',
+                  'fase ', 'Lazo de tensión sin estabilizar', f'{carpeta_salida}/lazo_V_B_sin_estabilizar.png')
+
+graficar_subplots(datos8[nombres8[0]], datos8[nombres8[1]], fase8_unwrap, 'Hz', 'dB',
+                  'fase ', 'Lazo de corriente sin estabilizar', f'{carpeta_salida}/lazo_I_B_sin_estabilizar.png')
